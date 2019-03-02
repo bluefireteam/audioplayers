17d2eb1f5ce5741201981708967e2b5a97c74ca8
jkclee123 <jkclee123@gmail.com>
2019-03-02 17:38:40 +0800

    android audio focus
    register AudioPlayersPlugin

--- example/android/app/src/main/java/xyz/luan/audioplayers_example/Application.java
+++ example/android/app/src/main/java/xyz/luan/audioplayers_example/Application.java
@@ -4,6 +4,7 @@ import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+
 import xyz.luan.audioplayers.AudioplayersPlugin;
 
 public class Application extends FlutterApplication implements PluginRegistrantCallback{
