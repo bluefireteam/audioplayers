ff4b20036940b8bf561431dba4dabbcca0b7914b
Andrew Leach <andrewleachy@gmail.com>
2019-11-20 11:40:13 +0100

    Anything

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/andrew/Downloads/flutter_linux_v1.1.8-beta/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/andrew/Documents/projects/opensource/audioplayers/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/andrew/Downloads/flutter_linux_v1.1.8-beta/flutter/bin/cache/artifacts/engine/ios"
