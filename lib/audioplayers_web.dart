c401a21e8552b521b4d6ed2f198056c9cb71fc7d
Luan Nico <luanpotter27@gmail.com>
2019-12-26 16:02:13 -0500

    test implementation

--- lib/audioplayers_web.dart
+++ lib/audioplayers_web.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'dart:html';
+import 'dart:web_audio';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 class AudioplayersPlugin {
+  static final AudioContext audioCtx = AudioContext();
+
   static void registerWith(Registrar registrar) {
     final MethodChannel channel = MethodChannel(
       'xyz.luan/audioplayers',
@@ -20,7 +24,21 @@ class AudioplayersPlugin {
     switch (method) {
       case 'play':
         final String url = call.arguments['url'];
-        print('hello from backend $url');
+        print('hello from backend $url $audioCtx');
+
+        print('get the audio file');
+        HttpRequest.request(url, responseType: "arraybuffer").then((HttpRequest request) {
+          print('decode it');
+          audioCtx.decodeAudioData(request.response).then((AudioBuffer buffer) {
+            print('play it now');
+            AudioBufferSourceNode source = audioCtx.createBufferSource();
+            source.buffer = buffer;
+            source.connectNode(audioCtx.destination);
+            source.start(audioCtx.currentTime);
+          });
+        });
+
+
         return 1;
       default:
         throw PlatformException(
