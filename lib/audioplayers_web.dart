207c39d8d295a7a561a2391d3e3968b3332a7f78
Luan Nico <luanpotter27@gmail.com>
2019-12-27 12:32:06 -0500

    adding more methods

--- lib/audioplayers_web.dart
+++ lib/audioplayers_web.dart
@@ -5,8 +5,71 @@ import 'dart:web_audio';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+final AudioContext _audioCtx = AudioContext();
+
+class WrappedPlayer {
+  double startingPoint;
+  double soughtPosition;
+  double pausedAt = null;
+  bool isPlaying = false;
+  AudioBuffer currentBuffer;
+  AudioBufferSourceNode currentNode;
+
+  void setBuffer(AudioBuffer buffer) {
+    stop();
+    currentBuffer = buffer;
+    recreateNode();
+    if (isPlaying) {
+      resume();
+    }
+  }
+
+  void recreateNode() {
+    currentNode = _audioCtx.createBufferSource();
+    currentNode.buffer = currentBuffer;
+    currentNode.connectNode(_audioCtx.destination);
+  }
+
+  void start(double position) {
+    isPlaying = true;
+    if (currentBuffer == null) {
+      return; // nothing to play yet
+    }
+    if (currentNode == null) {
+      recreateNode();
+    }
+    startingPoint = _audioCtx.currentTime;
+    soughtPosition = position;
+    currentNode.start(startingPoint, soughtPosition);
+  }
+
+  void resume() {
+    start(pausedAt ?? 0);
+  }
+
+  void pause() {
+    pausedAt = _audioCtx.currentTime - startingPoint + soughtPosition;
+    _cancel();
+  }
+
+  void stop() {
+    pausedAt = 0;
+    _cancel();
+  }
+
+  void _cancel() {
+    isPlaying = false;
+    currentNode?.stop();
+    currentNode = null;
+  }
+}
+
 class AudioplayersPlugin {
-  static final AudioContext audioCtx = AudioContext();
+  // players by playerId
+  Map<String, WrappedPlayer> players = {};
+
+  // cache of pre-loaded buffers by URL
+  Map<String, AudioBuffer> preloadedBuffers = {};
 
   static void registerWith(Registrar registrar) {
     final MethodChannel channel = MethodChannel(
@@ -19,27 +82,65 @@ class AudioplayersPlugin {
     channel.setMethodCallHandler(instance.handleMethodCall);
   }
 
+  Future<AudioBuffer> loadAudio(String url) async {
+    if (preloadedBuffers.containsKey(url)) {
+      return preloadedBuffers[url];
+    }
+
+    final HttpRequest response = await HttpRequest.request(url, responseType: "arraybuffer");
+    final AudioBuffer buffer = await _audioCtx.decodeAudioData(response.response);
+    return preloadedBuffers.putIfAbsent(url, () => buffer);
+  }
+
+  WrappedPlayer getOrCreatePlayer(String playerId) {
+    return players.putIfAbsent(playerId, () => WrappedPlayer());
+  }
+
+  Future<WrappedPlayer> setUrl(String playerId, String url) async {
+    final WrappedPlayer player = getOrCreatePlayer(playerId);
+    final AudioBuffer buffer = await loadAudio(url);
+    player.setBuffer(buffer);
+    return player;
+  }
+
   Future<dynamic> handleMethodCall(MethodCall call) async {
     final method = call.method;
+    final playerId = call.arguments['playerId'];
     switch (method) {
-      case 'play':
+      case 'setUrl': {
         final String url = call.arguments['url'];
-        print('hello from backend $url $audioCtx');
+        await setUrl(playerId, url);
+        return 1;
+      }
+      case 'play': {
+        final String url = call.arguments['url'];
+        final bool isLocal = call.arguments['isLocal'];
+        final double volume = call.arguments['volume'];
+        final double position = call.arguments['position'] ?? 0;
+        // web does not care for the `stayAwake` argument
 
-        print('get the audio file');
-        HttpRequest.request(url, responseType: "arraybuffer").then((HttpRequest request) {
-          print('decode it');
-          audioCtx.decodeAudioData(request.response).then((AudioBuffer buffer) {
-            print('play it now');
-            AudioBufferSourceNode source = audioCtx.createBufferSource();
-            source.buffer = buffer;
-            source.connectNode(audioCtx.destination);
-            source.start(audioCtx.currentTime);
-          });
-        });
-
+        final player = await setUrl(playerId, url);
+        player.start(position);
 
         return 1;
+      }
+      case 'pause': {
+        getOrCreatePlayer(playerId).pause();
+        return 1;
+      }
+      case 'stop': {
+        getOrCreatePlayer(playerId).stop();
+        return 1;
+      }
+      case 'resume': {
+        getOrCreatePlayer(playerId).resume();
+        return 1;
+      }
+      case 'release':
+      case 'seek':
+      case 'setVolume':
+      case 'setReleaseMode':
+      case 'setPlaybackRate':
       default:
         throw PlatformException(
           code: 'Unimplemented',
