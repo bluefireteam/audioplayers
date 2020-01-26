10014115e4032d505e6aab639da3361e75aeae27
david@missionctrlgames.com <david@missionctrlgames.com>
2020-01-26 07:33:08 -0500

    working on windows support

---
+++ windows/AudioPlayersPlugin.cpp
@@ -0,0 +1,128 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "AudioPlayersPlugin.h"
+
+#include <windows.h>
+#include <MMSystem.h>
+#include <VersionHelpers.h>
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar.h>
+#include <flutter/standard_method_codec.h>
+#include <memory>
+#include <sstream>
+
+namespace {
+
+class AudioPlayersPlugin : public flutter::Plugin {
+ public:
+  static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar);
+
+  // Creates a plugin that communicates on the given channel.
+  AudioPlayersPlugin(
+      std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel);
+
+  virtual ~AudioPlayersPlugin();
+
+ private:
+  // Called when a method is called on |channel_|;
+  void HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
+  // The MethodChannel used for communication with the Flutter engine.
+  std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
+};
+
+// static
+void AudioPlayersPlugin::RegisterWithRegistrar(flutter::PluginRegistrar *registrar) {
+  auto channel =
+      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+          registrar->messenger(), "AudioPlayersPlugin",
+          &flutter::StandardMethodCodec::GetInstance());
+  auto *channel_pointer = channel.get();
+
+  auto plugin = std::make_unique<AudioPlayersPlugin>(std::move(channel));
+
+  channel_pointer->SetMethodCallHandler(
+      [plugin_pointer = plugin.get()](const auto &call, auto result) {
+        plugin_pointer->HandleMethodCall(call, std::move(result));
+      });
+
+  registrar->AddPlugin(std::move(plugin));
+}
+
+AudioPlayersPlugin::AudioPlayersPlugin(
+    std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel)
+    : channel_(std::move(channel)) {}
+
+AudioPlayersPlugin::~AudioPlayersPlugin(){};
+
+void AudioPlayersPlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (!method_call.arguments() || method_call.arguments()->IsNull()) {
+    result->Error(kBadArgumentError, "Method invoked without args");
+    return;
+  }
+  if (method_call.method_name().compare("getPlatformVersion") == 0) {
+    const rapidjson::Document& args = *method_call.arguments();
+    std::ostringstream version_stream;
+    version_stream << "Windows ";
+	// The result returned here will depend on the app manifest of the runner.
+    if (IsWindows10OrGreater()) {
+      version_stream << "10+";
+    } else if (IsWindows8OrGreater()) {
+      version_stream << "8";
+    } else if (IsWindows7OrGreater()) {
+      version_stream << "7";
+    }
+    flutter::EncodableValue response(version_stream.str());
+    result->Success(&response);
+  } else if (method_call.method_name().compare("play") == 0) {
+    auto url = method_call.args.FindMember("url");
+    PlaySound(TEXT(url), NULL, SND_FILENAME);
+    result->Success(&response);
+  } else if (method_call.method_name().compare("pause") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("resume") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("stop") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("release") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("seek") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("setUrl") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("getDuration") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("setVolume") == 0) {
+    result->Success(&response);
+  } else if (method_call.method_name().compare("setReleaseMode") == 0) {
+    result->Success(&response);
+  } else {
+    result->NotImplemented();
+  }
+}
+
+}  // namespace
+
+void AudioPlayersPluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  // The plugin registrar owns the plugin, registered callbacks, etc., so must
+  // remain valid for the life of the application.
+  static auto *plugin_registrar = new flutter::PluginRegistrar(registrar);
+
+  AudioPlayersPlugin::RegisterWithRegistrar(plugin_registrar);
+}
