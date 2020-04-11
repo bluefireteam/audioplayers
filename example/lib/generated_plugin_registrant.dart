93cc35bebc90f28059011ccf96ad730bfe207e8a
Luan Nico <luanpotter27@gmail.com>
2020-04-10 22:38:33 -0400

    Fix to work on release mode

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -9,6 +9,7 @@ import 'package:audioplayers/audioplayers_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   AudioplayersPlugin.registerWith(registry.registrarFor(AudioplayersPlugin));
   registry.registerMessageHandler();
