811f0587e93380e3a6f197e9d663d1f3abcd3a97
majing <majing@aragoncs.com>
2020-12-07 19:53:28 +0800

    don't show alert when loading

--- darwin/Classes/AudioplayersPlugin.m
+++ darwin/Classes/AudioplayersPlugin.m
@@ -354,7 +354,7 @@ NSString *_playerIndex;
                           int elapsedTime = [call.arguments[@"elapsedTime"] intValue];
                           bool enablePreviousTrackButton = [call.arguments[@"hasPreviousTrack"] boolValue];
                           bool enableNextTrackButton = [call.arguments[@"hasNextTrack"] boolValue];
-
+                      bool isPlay = [call.arguments[@"isPlay"] boolValue];
                           [self setNotification:title
                                      albumTitle:albumTitle
                                          artist:artist
@@ -365,7 +365,7 @@ NSString *_playerIndex;
                                     elapsedTime:elapsedTime
                            enablePreviousTrackButton:enablePreviousTrackButton
                           enableNextTrackButton:enableNextTrackButton
-                                       playerId:playerId];
+                                       playerId:playerId isPlay:isPlay];
                       #else
                           result(FlutterMethodNotImplemented);
                       #endif
@@ -421,7 +421,8 @@ NSString *_playerIndex;
             elapsedTime:  (int) elapsedTime
             enablePreviousTrackButton: (BOOL)enablePreviousTrackButton
             enableNextTrackButton: (BOOL)enableNextTrackButton
-            playerId: (NSString*) playerId {
+            playerId: (NSString*) playerId
+                     isPlay:(bool)isPlay{
         _title = title;
         _albumTitle = albumTitle;
         _artist = artist;
@@ -430,7 +431,7 @@ NSString *_playerIndex;
 
         _infoCenter = [MPNowPlayingInfoCenter defaultCenter];
         
-        [ self updateNotification:elapsedTime ];
+        [ self updateNotification:elapsedTime isPlay:isPlay];
 
         if (remoteCommandCenter == nil) {
           remoteCommandCenter = [MPRemoteCommandCenter sharedCommandCenter];
@@ -597,7 +598,7 @@ NSString *_playerIndex;
         return MPRemoteCommandHandlerStatusSuccess;
     }
 
-    -(void) updateNotification: (int) elapsedTime {
+-(void) updateNotification: (int) elapsedTime isPlay:(bool)isPlay{
       NSMutableDictionary *playingInfo = [NSMutableDictionary dictionary];
       playingInfo[MPMediaItemPropertyTitle] = _title;
       playingInfo[MPMediaItemPropertyAlbumTitle] = _albumTitle;
@@ -617,10 +618,10 @@ NSString *_playerIndex;
           playingInfo[MPMediaItemPropertyPlaybackDuration] = [NSNumber numberWithInt: _duration];
             // From `MPNowPlayingInfoPropertyElapsedPlaybackTime` docs -- it is not recommended to update this value frequently. Thus it should represent integer seconds and not an accurate `CMTime` value with fractions of a second
           
-          NSMutableDictionary * playerInfo = players[_currentPlayerId];
-          AVPlayer *player = playerInfo[@"player"];
-          playingInfo[MPNowPlayingInfoPropertyPlaybackRate] = @(player.rate);
-          playingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = [NSNumber numberWithInt: CMTimeGetSeconds(player.currentTime)];
+          float rate = isPlay ? 1.0 : 0.0;
+          playingInfo[MPNowPlayingInfoPropertyPlaybackRate] = @(rate);
+          
+          playingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = [NSNumber numberWithInt: elapsedTime];
           NSLog(@"setNotification done");
 
           if (_infoCenter != nil) {
@@ -687,7 +688,7 @@ NSString *_playerIndex;
       
     if (playerInfo[@"url"]) {
       [[player currentItem] removeObserver:self forKeyPath:@"status" ];
-        [[player currentItem] removeObserver:self forKeyPath:@"playbackBufferEmpty" ];
+//        [[player currentItem] removeObserver:self forKeyPath:@"playbackBufferEmpty" ];
       [ playerInfo setObject:url forKey:@"url" ];
 
       for (id ob in observers) {
@@ -697,11 +698,11 @@ NSString *_playerIndex;
       [ player replaceCurrentItemWithPlayerItem: playerItem ];
     } else {
       player = [[ AVPlayer alloc ] initWithPlayerItem: playerItem ];
-        if (@available(iOS 10.0, *)) {
-            player.automaticallyWaitsToMinimizeStalling = NO;
-        } else {
-            // Fallback on earlier versions
-        }
+//        if (@available(iOS 10.0, *)) {
+//            player.automaticallyWaitsToMinimizeStalling = NO;
+//        } else {
+//            // Fallback on earlier versions
+//        }
 
       observers = [[NSMutableSet alloc] init];
 
@@ -731,7 +732,7 @@ NSString *_playerIndex;
                           forKeyPath:@"status"
                           options:0
                           context:(void*)playerId];
-    [playerItem addObserver:self forKeyPath:@"playbackBufferEmpty" options:0 context:(void*)playerId];
+//    [playerItem addObserver:self forKeyPath:@"playbackBufferEmpty" options:0 context:(void*)playerId];
       
 //  } else {
 //    if ([[player currentItem] status ] == AVPlayerItemStatusReadyToPlay) {
@@ -887,7 +888,8 @@ recordingActive: (bool) recordingActive
       if (_infoCenter != nil) {
         AVPlayerItem *currentItem = player.currentItem;
         CMTime currentTime = currentItem.currentTime;
-        [ self updateNotification:CMTimeGetSeconds(currentTime) ];
+          BOOL isPlay = [playerInfo[@"isPlaying"] boolValue];
+        [ self updateNotification:CMTimeGetSeconds(currentTime) isPlay:isPlay];
       }
   #endif
 }
@@ -937,7 +939,8 @@ recordingActive: (bool) recordingActive
           NSLog(@"ios -> seekComplete...");
           int seconds = CMTimeGetSeconds(time);
           if (_infoCenter != nil) {
-            [ self updateNotification:seconds ];
+              bool isPlay = [playerInfo[@"isPlaying"] boolValue];
+            [ self updateNotification:seconds isPlay:isPlay];
           }
           [ _channel_audioplayer invokeMethod:@"audio.onSeekComplete" arguments:@{@"playerId": playerId,@"value":@(YES)}];
       }else{
@@ -1006,18 +1009,18 @@ recordingActive: (bool) recordingActive
         NSLog(@"========== error  1");
       [_channel_audioplayer invokeMethod:@"audio.onError" arguments:@{@"playerId": playerId, @"value": @"AVPlayerItemStatus.failed"}];
     }
-  }else if([keyPath isEqualToString:@"playbackBufferEmpty"]){
-      NSMutableDictionary * playerInfo = players[_currentPlayerId];
-      AVPlayer *player = playerInfo[@"player"];
-      if([player currentItem].playbackBufferEmpty == YES){
-          NSLog(@"========== error  2");
-          [_channel_audioplayer invokeMethod:@"audio.onError" arguments:@{@"playerId": _currentPlayerId, @"value": @"AVPlayerItemStatus.failed"}];
-          NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithDictionary:[_infoCenter nowPlayingInfo]];
-          [dict setObject:@(CMTimeGetSeconds(player.currentTime)) forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
-          
-              [dict setObject:@(0) forKey:MPNowPlayingInfoPropertyPlaybackRate];
-              [_infoCenter setNowPlayingInfo:dict];
-      }
+//  }else if([keyPath isEqualToString:@"playbackBufferEmpty"]){
+//      NSMutableDictionary * playerInfo = players[_currentPlayerId];
+//      AVPlayer *player = playerInfo[@"player"];
+//      if([player currentItem].playbackBufferEmpty == YES){
+//          NSLog(@"========== error  2");
+//          [_channel_audioplayer invokeMethod:@"audio.onError" arguments:@{@"playerId": _currentPlayerId, @"value": @"AVPlayerItemStatus.failed"}];
+//          NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithDictionary:[_infoCenter nowPlayingInfo]];
+//          [dict setObject:@(CMTimeGetSeconds(player.currentTime)) forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
+//
+//              [dict setObject:@(0) forKey:MPNowPlayingInfoPropertyPlaybackRate];
+//              [_infoCenter setNowPlayingInfo:dict];
+//      }
   }else {
     // Any unrecognized context must belong to super
     [super observeValueForKeyPath:keyPath
