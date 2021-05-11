f46bc5f5f4b928dc828ba82521d235d6952d2a53
Luan Nico <luanpotter27@gmail.com>
2021-05-11 08:28:11 -0400

    Fix local files for macos

--- packages/audioplayers/darwin/Classes/Utils.swift
+++ packages/audioplayers/darwin/Classes/Utils.swift
@@ -1,5 +1,12 @@
 import AVKit
 
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+}
+
 class Logger {
     // TODO(luan) wire this with the Dart side
     static var enableLogs = false
