854d974731291f3264d8640f84de341b1fa4e29c
Luan Nico <luanpotter27@gmail.com>
2021-12-30 10:36:18 -0500

    Re-add integration tests

---
+++ packages/audioplayers/example/integration_test/app_test.dart
@@ -0,0 +1,20 @@
+import 'package:audioplayers_example/main.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('verify app is launched', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      expect(
+        find.text(
+          'Sample 1 (https://luan.xyz/files/audio/ambient_c_motion.mp3)',
+        ),
+        findsOneWidget,
+      );
+    });
+  });
+}
