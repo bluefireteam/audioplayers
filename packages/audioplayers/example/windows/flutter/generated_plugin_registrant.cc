83026acddb265dbab1fd09f06d3ac20dade6533c
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2022-01-26 17:53:26 -0300

    Initial support for Windows

---
+++ packages/audioplayers/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers/audioplayers_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersPlugin"));
+}
