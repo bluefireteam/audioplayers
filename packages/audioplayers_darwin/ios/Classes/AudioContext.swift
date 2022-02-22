66d9689c96b1da50d501bd3408c9b99a6503aa8b
Luan Nico <luanpotter27@gmail.com>
2022-02-21 20:37:01 -0500

    Add equals method to ios Audio Context

--- packages/audioplayers_darwin/ios/Classes/AudioContext.swift
+++ packages/audioplayers_darwin/ios/Classes/AudioContext.swift
@@ -1,6 +1,6 @@
 import MediaPlayer
 
-struct AudioContext {
+struct AudioContext : Equatable {
     let category: AVAudioSession.Category
     let options: [AVAudioSession.CategoryOptions]
     let defaultToSpeaker: Bool
@@ -19,5 +19,9 @@ struct AudioContext {
         self.category = category
         self.options = options
         self.defaultToSpeaker = defaultToSpeaker
+    }
+    
+    static func ==(o1: AudioContext, o2: AudioContext) -> Bool {
+        return o1.defaultToSpeaker == o2.defaultToSpeaker && o1.category == o2.category && o1.options == o2.options
     }
 }
