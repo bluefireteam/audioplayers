2b66fff610a6ee0a70557cad036ab931f17fc51c
Alexandre Zollinger Chohfi <chohfi@outlook.com>
2022-03-26 13:22:08 -0700

    Initial support for Windows (#1068)
    * Initial support for Windows
    
    * Added Windows build github job.
    
    * Windows now linking to windowsapp lib
    
    * Merge main
    
    * Update packages/audioplayers/pubspec.yaml
    
    * Apply suggestions from code review
    
    * Update packages/audioplayers_windows/windows/audio_player.h
    
    Co-authored-by: Luan Nico <luanpotter27@gmail.com>

---
+++ packages/audioplayers/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_windows/audioplayers_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+}
