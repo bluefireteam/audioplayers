5017d9c49b6e993017881db8dee0694147ef63cf
Luan Nico <luanpotter27@gmail.com>
2022-04-30 11:50:33 -0400

    Add timeout

--- packages/audioplayers/example/integration_test/app_test.dart
+++ packages/audioplayers/example/integration_test/app_test.dart
@@ -6,13 +6,17 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('verify app is launched', (WidgetTester tester) async {
-      app.main();
-      await tester.pumpAndSettle();
-      expect(
-        find.text('Remote URL WAV 1 - coins.wav'),
-        findsOneWidget,
-      );
-    });
+    testWidgets(
+      'verify app is launched',
+      (WidgetTester tester) async {
+        app.main();
+        await tester.pumpAndSettle();
+        expect(
+          find.text('Remote URL WAV 1 - coins.wav'),
+          findsOneWidget,
+        );
+      },
+      timeout: const Timeout(Duration(minutes: 1)),
+    );
   });
 }
