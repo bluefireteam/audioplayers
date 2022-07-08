6e7a74b24854ec18464ac5e5fc0bcb3e68cb41ed
Gustl22 <git@reb0.org>
2022-07-08 14:16:13 +0200

    test: reorganize code

--- packages/audioplayers/example/integration_test/app_test.dart
+++ packages/audioplayers/example/integration_test/app_test.dart
@@ -1,72 +1,15 @@
-import 'dart:async';
-import 'dart:io' show Platform;
-
 import 'package:audioplayers_example/main.dart' as app;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-final PlatformFeatures features = kIsWeb
-    ? PlatformFeatures(
-        hasBytesSource: false,
-        hasPlaylistSourceType: false,
-        hasLowLatency: false,
-        hasReleaseMode: false,
-        hasSeek: false,
-        hasDuckAudio: false,
-        hasRespectSilence: false,
-        hasStayAwake: false,
-        hasRecordingActive: false,
-        hasPlayingRoute: false,
-        hasDurationEvent: false,
-        hasCompletionEvent: false,
-        hasErrorEvent: false,
-      )
-    : Platform.isAndroid
-        ? PlatformFeatures(
-            hasRecordingActive: false,
-          )
-        : Platform.isIOS
-            ? PlatformFeatures(
-                hasBytesSource: false,
-                hasLowLatency: false,
-                hasDuckAudio: false,
-              )
-            : Platform.isMacOS
-                ? PlatformFeatures(
-                    hasBytesSource: false,
-                    hasLowLatency: false,
-                    hasDuckAudio: false,
-                    hasRespectSilence: false,
-                    hasStayAwake: false,
-                    hasRecordingActive: false,
-                    hasPlayingRoute: false,
-                  )
-                : Platform.isLinux
-                    ? PlatformFeatures(
-                        hasBytesSource: false,
-                        hasLowLatency: false,
-                        hasMp3Duration: false,
-                        hasDuckAudio: false,
-                        hasRespectSilence: false,
-                        hasStayAwake: false,
-                        hasRecordingActive: false,
-                        hasPlayingRoute: false,
-                      )
-                    : Platform.isWindows
-                        ? PlatformFeatures(
-                            hasBytesSource: false,
-                            hasLowLatency: false,
-                            hasDuckAudio: false,
-                            hasRespectSilence: false,
-                            hasStayAwake: false,
-                            hasRecordingActive: false,
-                            hasPlayingRoute: false,
-                          )
-                        : PlatformFeatures();
+import 'platform_features.dart';
+import 'source_test_data.dart';
+import 'test_utils.dart';
 
 void main() {
+  final features = PlatformFeatures.instance();
+
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
@@ -192,7 +135,9 @@ void main() {
 
         // Test if onDurationText is set immediately.
         if (!audioSourceTestData.isStream && isImmediateDurationSupported) {
-          await tester.testOnDuration(audioSourceTestData);
+          if (features.hasDurationEvent) {
+            await tester.testOnDuration(audioSourceTestData);
+          }
         }
 
         const sampleDuration = Duration(seconds: 2);
@@ -203,7 +148,9 @@ void main() {
           // Cannot test more precisely as initialization takes some time and
           // a longer sampleDuration would decelerate length of overall tests.
           // TODO(Gustl22): test position update in seek mode.
-          await tester.testOnPosition('0:00:0');
+          if (features.hasPositionEvent) {
+            await tester.testOnPosition('0:00:0');
+          }
         }
 
         // Display duration after end / stop (some samples are shorter than sampleDuration, so this test would fail)
@@ -238,238 +185,5 @@ void main() {
   group('play multiple sources', () {
     // TODO(Gustl22): play sources simultaneously
     // TODO(Gustl22): play one source after another
-  });
-}
-
-extension on WidgetTester {
-  Future<void> testDuration(SourceTestData sourceTestData) async {
-    await tap(find.byKey(const Key('getDuration')));
-    await pumpAndSettle();
-    expectWidgetHasText(
-      const Key('durationText'),
-      // Precision for duration:
-      // Android: hundredth of a second
-      // Windows: second
-      matcher: contains(
-        sourceTestData.duration.toString().substring(0, 8),
-      ),
-    );
-  }
-
-  Future<void> testPosition(String positionStr) async {
-    await tap(find.byKey(const Key('getPosition')));
-    await pumpAndSettle();
-    expectWidgetHasText(
-      const Key('positionText'),
-      matcher: contains(positionStr),
-    );
-  }
-
-  Future<void> testOnDuration(SourceTestData sourceTestData) async {
-    if (features.hasDurationEvent) {
-      final durationStr = sourceTestData.duration.toString().substring(0, 8);
-      final currentDurationStr = (find
-              .byKey(const Key('onDurationText'))
-              .evaluate()
-              .single
-              .widget as Text)
-          .data;
-      await waitFor(
-        () => expectWidgetHasText(
-          const Key('onDurationText'),
-          matcher: contains(
-            'Stream Duration: $durationStr',
-          ),
-        ),
-        stackTrace: [
-          StackTrace.current.toString(),
-          'Current: $currentDurationStr',
-          'Expected: $durationStr',
-        ],
-      );
-    }
-  }
-
-  Future<void> testOnPosition(String positionStr) async {
-    if (features.hasPositionEvent) {
-      final currentPositionStr = (find
-              .byKey(const Key('onPositionText'))
-              .evaluate()
-              .single
-              .widget as Text)
-          .data;
-      await waitFor(
-        () => expectWidgetHasText(
-          const Key('onPositionText'),
-          matcher: contains('Stream Position: $positionStr'),
-        ),
-        stackTrace: [
-          StackTrace.current.toString(),
-          'Current: $currentPositionStr',
-          'Expected: $positionStr',
-        ],
-      );
-    }
-  }
-
-  // Add [stackTrace] to work around https://github.com/flutter/flutter/issues/89138
-  Future<void> waitFor(
-    void Function() testExpectation, {
-    Duration? timeout = const Duration(seconds: 15),
-    List<String>? stackTrace,
-  }) =>
-      _waitUntil(
-        () async {
-          try {
-            await pumpAndSettle();
-            testExpectation();
-            return true;
-          } on TestFailure {
-            return false;
-          }
-        },
-        timeout: timeout,
-        stackTrace: stackTrace,
-      );
-
-  /// Waits until the [condition] returns true
-  /// Will raise a complete with a [TimeoutException] if the
-  /// condition does not return true with the timeout period.
-  /// Copied from: https://github.com/jonsamwell/flutter_gherkin/blob/02a4af91d7a2512e0a4540b9b1ab13e36d5c6f37/lib/src/flutter/utils/driver_utils.dart#L86
-  Future<void> _waitUntil(
-    Future<bool> Function() condition, {
-    Duration? timeout = const Duration(seconds: 15),
-    Duration? pollInterval = const Duration(milliseconds: 500),
-    List<String>? stackTrace,
-  }) async {
-    try {
-      await Future.microtask(
-        () async {
-          final completer = Completer<void>();
-          final maxAttempts =
-              (timeout!.inMilliseconds / pollInterval!.inMilliseconds).round();
-          var attempts = 0;
-
-          while (attempts < maxAttempts) {
-            final result = await condition();
-            if (result) {
-              completer.complete();
-              break;
-            } else {
-              await Future<void>.delayed(pollInterval);
-            }
-            attempts++;
-          }
-        },
-      ).timeout(
-        timeout!,
-      );
-    } on TimeoutException catch (e) {
-      throw Exception('$e\nStacktrace:\n${stackTrace?.join('\n')}');
-    }
-  }
-
-  Future<void> scrollTo(Key widgetKey) async {
-    await dragUntilVisible(
-      find.byKey(widgetKey),
-      find.byType(SingleChildScrollView).first,
-      const Offset(0, 100),
-    );
-    await pumpAndSettle();
-  }
-}
-
-void expectWidgetHasText(
-  Key key, {
-  required Matcher matcher,
-  bool skipOffstage = true,
-}) {
-  final widget =
-      find.byKey(key, skipOffstage: skipOffstage).evaluate().single.widget;
-  if (widget is Text) {
-    expect(widget.data, matcher);
-  } else {
-    throw 'Widget with key $key is not a Widget of type "Text"';
-  }
-}
-
-enum UserPlatform {
-  android,
-  fuchsia,
-  iOS,
-  linux,
-  macOS,
-  windows,
-  web,
-}
-
-class SourceTestData {
-  String sourceKey;
-
-  Duration duration;
-
-  bool isStream;
-
-  SourceTestData({
-    required this.sourceKey,
-    required this.duration,
-    this.isStream = false,
-  });
-
-  @override
-  String toString() {
-    return 'SourceTestData('
-        'sourceKey: $sourceKey, '
-        'duration: $duration, '
-        'isStream: $isStream'
-        ')';
-  }
-}
-
-class PlatformFeatures {
-  final bool hasUrlSource;
-  final bool hasAssetSource;
-  final bool hasBytesSource;
-
-  final bool hasPlaylistSourceType;
-
-  final bool hasLowLatency; // Not yet tested
-  final bool hasReleaseMode; // Not yet tested
-  final bool hasVolume; // Not yet tested
-  final bool hasSeek; // Not yet tested
-  final bool hasMp3Duration; // Not yet tested
-
-  final bool hasPlaybackRate; // Not yet tested
-  final bool hasDuckAudio; // Not yet tested
-  final bool hasRespectSilence; // Not yet tested
-  final bool hasStayAwake; // Not yet tested
-  final bool hasRecordingActive; // Not yet tested
-  final bool hasPlayingRoute; // Not yet tested
-
-  final bool hasDurationEvent;
-  final bool hasPositionEvent;
-  final bool hasCompletionEvent; // Not yet tested
-  final bool hasErrorEvent; // Not yet tested
-
-  PlatformFeatures({
-    this.hasUrlSource = true,
-    this.hasAssetSource = true,
-    this.hasBytesSource = true,
-    this.hasPlaylistSourceType = true,
-    this.hasLowLatency = true,
-    this.hasReleaseMode = true,
-    this.hasMp3Duration = true,
-    this.hasVolume = true,
-    this.hasSeek = true,
-    this.hasPlaybackRate = true,
-    this.hasDuckAudio = true,
-    this.hasRespectSilence = true,
-    this.hasStayAwake = true,
-    this.hasRecordingActive = true,
-    this.hasPlayingRoute = true,
-    this.hasDurationEvent = true,
-    this.hasPositionEvent = true,
-    this.hasCompletionEvent = true,
-    this.hasErrorEvent = true,
   });
 }
---
+++ packages/audioplayers/example/integration_test/platform_features.dart
@@ -0,0 +1,125 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+/// Specify supported features for a platform.
+class PlatformFeatures {
+  static const webPlatformFeatures = PlatformFeatures(
+    hasBytesSource: false,
+    hasPlaylistSourceType: false,
+    hasLowLatency: false,
+    hasReleaseMode: false,
+    hasSeek: false,
+    hasDuckAudio: false,
+    hasRespectSilence: false,
+    hasStayAwake: false,
+    hasRecordingActive: false,
+    hasPlayingRoute: false,
+    hasDurationEvent: false,
+    hasCompletionEvent: false,
+    hasErrorEvent: false,
+  );
+
+  static const androidPlatformFeatures = PlatformFeatures(
+    hasRecordingActive: false,
+  );
+
+  static const iosPlatformFeatures = PlatformFeatures(
+    hasBytesSource: false,
+    hasLowLatency: false,
+    hasDuckAudio: false,
+  );
+
+  static const macPlatformFeatures = PlatformFeatures(
+    hasBytesSource: false,
+    hasLowLatency: false,
+    hasDuckAudio: false,
+    hasRespectSilence: false,
+    hasStayAwake: false,
+    hasRecordingActive: false,
+    hasPlayingRoute: false,
+  );
+
+  static const linuxPlatformFeatures = PlatformFeatures(
+    hasBytesSource: false,
+    hasLowLatency: false,
+    hasMp3Duration: false,
+    hasDuckAudio: false,
+    hasRespectSilence: false,
+    hasStayAwake: false,
+    hasRecordingActive: false,
+    hasPlayingRoute: false,
+  );
+
+  static const windowsPlatformFeatures = PlatformFeatures(
+    hasBytesSource: false,
+    hasLowLatency: false,
+    hasDuckAudio: false,
+    hasRespectSilence: false,
+    hasStayAwake: false,
+    hasRecordingActive: false,
+    hasPlayingRoute: false,
+  );
+
+  final bool hasUrlSource;
+  final bool hasAssetSource;
+  final bool hasBytesSource;
+
+  final bool hasPlaylistSourceType;
+
+  final bool hasLowLatency; // Not yet tested
+  final bool hasReleaseMode; // Not yet tested
+  final bool hasVolume; // Not yet tested
+  final bool hasSeek; // Not yet tested
+  final bool hasMp3Duration; // Not yet tested
+
+  final bool hasPlaybackRate; // Not yet tested
+  final bool hasDuckAudio; // Not yet tested
+  final bool hasRespectSilence; // Not yet tested
+  final bool hasStayAwake; // Not yet tested
+  final bool hasRecordingActive; // Not yet tested
+  final bool hasPlayingRoute; // Not yet tested
+
+  final bool hasDurationEvent;
+  final bool hasPositionEvent;
+  final bool hasCompletionEvent; // Not yet tested
+  final bool hasErrorEvent; // Not yet tested
+
+  const PlatformFeatures({
+    this.hasUrlSource = true,
+    this.hasAssetSource = true,
+    this.hasBytesSource = true,
+    this.hasPlaylistSourceType = true,
+    this.hasLowLatency = true,
+    this.hasReleaseMode = true,
+    this.hasMp3Duration = true,
+    this.hasVolume = true,
+    this.hasSeek = true,
+    this.hasPlaybackRate = true,
+    this.hasDuckAudio = true,
+    this.hasRespectSilence = true,
+    this.hasStayAwake = true,
+    this.hasRecordingActive = true,
+    this.hasPlayingRoute = true,
+    this.hasDurationEvent = true,
+    this.hasPositionEvent = true,
+    this.hasCompletionEvent = true,
+    this.hasErrorEvent = true,
+  });
+
+  factory PlatformFeatures.instance() {
+    return kIsWeb
+        ? webPlatformFeatures
+        : Platform.isAndroid
+            ? androidPlatformFeatures
+            : Platform.isIOS
+                ? iosPlatformFeatures
+                : Platform.isMacOS
+                    ? macPlatformFeatures
+                    : Platform.isLinux
+                        ? linuxPlatformFeatures
+                        : Platform.isWindows
+                            ? windowsPlatformFeatures
+                            : const PlatformFeatures();
+  }
+}
