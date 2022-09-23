48cf902b72e795f173bf531eaedf75b19d3b7dba
Gustl22 <git@reb0.org>
2022-09-23 15:25:01 +0200

    Update packages/audioplayers_platform_interface/lib/method_channel_audioplayers_platform.dart

--- packages/audioplayers_platform_interface/lib/method_channel_audioplayers_platform.dart
+++ packages/audioplayers_platform_interface/lib/method_channel_audioplayers_platform.dart
@@ -5,7 +5,6 @@ import 'dart:typed_data';
 
 import 'package:audioplayers_platform_interface/api/audio_context_config.dart';
 import 'package:audioplayers_platform_interface/api/player_mode.dart';
-import 'package:audioplayers_platform_interface/api/player_state.dart';
 import 'package:audioplayers_platform_interface/api/release_mode.dart';
 import 'package:audioplayers_platform_interface/audioplayers_platform_interface.dart';
 import 'package:audioplayers_platform_interface/logger_platform_interface.dart';
