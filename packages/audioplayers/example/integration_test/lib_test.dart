0b28d649e645d986e64691facc5deb0b9dac94da
Gustl22 <git@reb0.org>
2022-12-14 11:59:26 +0100

    fix: add missing IntegrationTestWidgetsFlutterBinding

--- packages/audioplayers/example/integration_test/lib_test.dart
+++ packages/audioplayers/example/integration_test/lib_test.dart
@@ -1,12 +1,15 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers_example/tabs/sources.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 import 'platform_features.dart';
 import 'source_test_data.dart';
 
 void main() {
   final features = PlatformFeatures.instance();
+
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('play multiple sources', () {
     final audioTestDataList = [
@@ -56,52 +59,46 @@ void main() {
 
     testWidgets('play multiple sources simultaneously',
         (WidgetTester tester) async {
-      // TODO(gustl22): Wrap tests inside async call for web, as completer does not complete on setSource: https://github.com/flutter/flutter/issues/116984
-      await tester.runAsync(() async {
-        final players =
-            List.generate(audioTestDataList.length, (_) => AudioPlayer());
+      final players =
+          List.generate(audioTestDataList.length, (_) => AudioPlayer());
 
-        // Start all players simultaneously
-        final iterator = List<int>.generate(audioTestDataList.length, (i) => i);
-        await Future.wait<void>(
-          iterator.map((i) => players[i].play(audioTestDataList[i].source)),
-        );
-        await tester.pumpAndSettle();
-        await tester.pump(const Duration(seconds: 8));
-        for (var i = 0; i < audioTestDataList.length; i++) {
-          final td = audioTestDataList[i];
-          if (td.isLiveStream || td.duration > const Duration(seconds: 10)) {
-            await tester.pump();
-            final position = await players[i].getCurrentPosition();
-            printOnFailure('Test position: $td');
-            expect(position, greaterThan(Duration.zero));
-          }
-          await players[i].stop();
+      // Start all players simultaneously
+      final iterator = List<int>.generate(audioTestDataList.length, (i) => i);
+      await Future.wait<void>(
+        iterator.map((i) => players[i].play(audioTestDataList[i].source)),
+      );
+      await tester.pumpAndSettle();
+      await tester.pump(const Duration(seconds: 8));
+      for (var i = 0; i < audioTestDataList.length; i++) {
+        final td = audioTestDataList[i];
+        if (td.isLiveStream || td.duration > const Duration(seconds: 10)) {
+          await tester.pump();
+          final position = await players[i].getCurrentPosition();
+          printOnFailure('Test position: $td');
+          expect(position, greaterThan(Duration.zero));
         }
-      });
+        await players[i].stop();
+      }
     });
 
     testWidgets('play multiple sources consecutively',
         (WidgetTester tester) async {
-      // TODO(gustl22): see https://github.com/flutter/flutter/issues/116984
-      await tester.runAsync(() async {
-        final player = AudioPlayer();
+      final player = AudioPlayer();
 
-        for (var i = 0; i < audioTestDataList.length; i++) {
-          final td = audioTestDataList[i];
-          await player.play(td.source);
-          await tester.pumpAndSettle();
-          // Live stream takes some time to get initialized
-          await tester.pump(Duration(seconds: td.isLiveStream ? 8 : 2));
-          if (td.isLiveStream || td.duration > const Duration(seconds: 10)) {
-            await tester.pump();
-            final position = await player.getCurrentPosition();
-            printOnFailure('Test position: $td');
-            expect(position, greaterThan(Duration.zero));
-          }
-          await player.stop();
+      for (var i = 0; i < audioTestDataList.length; i++) {
+        final td = audioTestDataList[i];
+        await player.play(td.source);
+        await tester.pumpAndSettle();
+        // Live stream takes some time to get initialized
+        await tester.pump(Duration(seconds: td.isLiveStream ? 8 : 2));
+        if (td.isLiveStream || td.duration > const Duration(seconds: 10)) {
+          await tester.pump();
+          final position = await player.getCurrentPosition();
+          printOnFailure('Test position: $td');
+          expect(position, greaterThan(Duration.zero));
         }
-      });
+        await player.stop();
+      }
     });
   });
 }
