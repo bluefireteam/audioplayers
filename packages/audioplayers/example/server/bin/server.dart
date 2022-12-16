77add1dc44d663245b0e4189b7a17f1e8a7b734c
Gustl22 <git@reb0.org>
2022-12-16 14:54:43 +0100

    CI: run via test server

--- packages/audioplayers/example/server/bin/server.dart
+++ packages/audioplayers/example/server/bin/server.dart
@@ -12,14 +12,29 @@ Future<void> main() async {
   // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
   // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final requestTimeoutMillis =
+      int.parse(Platform.environment['REQUEST_TIMEOUT'] ?? '0');
 
   final cascade = Cascade().add(_staticHandler);
 
+  final handler = const Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(
+        (innerHandler) => ((req) async {
+          await Future.delayed(Duration(milliseconds: requestTimeoutMillis));
+          return await innerHandler(req);
+        }),
+      )
+      .addHandler(cascade.handler);
+
   final server = await shelf_io.serve(
-    logRequests().addHandler(cascade.handler),
+    handler,
     InternetAddress.loopbackIPv4,
     port,
   );
+
+  // TODO(Gustl22): provide an audio streaming endpoint:
+  // Inspiration: https://github.com/daspinola/video-stream-sample/blob/master/server.js
 
   print('Serving at http://${server.address.host}:${server.port}');
 }
