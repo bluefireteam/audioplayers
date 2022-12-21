11e96b69ac096100a79124ae9f8479ed333aca62
Gustl22 <git@reb0.org>
2022-12-21 13:22:56 +0100

    feat: remove docs / comments from server

--- packages/audioplayers/example/server/bin/server.dart
+++ packages/audioplayers/example/server/bin/server.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
@@ -9,8 +5,6 @@ import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
 Future<void> main() async {
-  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
-  // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final requestTimeoutMillis =
       int.parse(Platform.environment['REQUEST_TIMEOUT'] ?? '0');
@@ -39,7 +33,6 @@ Future<void> main() async {
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-// Serve files from the file system.
 final _staticHandler = shelf_static.createStaticHandler(
   'public',
   defaultDocument: 'index.html',
