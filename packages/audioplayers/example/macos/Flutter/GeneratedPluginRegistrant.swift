9edf18ed34b16e2785c1045be3e1c70ca5684b28
Lukas Klingsbo <me@lukas.fyi>
2023-01-28 01:51:12 +0100

    Bump some more dependencies

--- packages/audioplayers/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/audioplayers/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import path_provider_foundation
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
