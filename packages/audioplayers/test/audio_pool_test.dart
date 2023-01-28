571d4cc6434a9ec64b7360a7e8ec273a5782cff7
Lukas Klingsbo <me@lukas.fyi>
2023-01-28 01:22:38 +0100

    feat: AudioPool (moved and improved from flame_audio)

---
+++ packages/audioplayers/test/audio_pool_test.dart
@@ -0,0 +1,68 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const _channel = MethodChannel('plugins.flutter.io/path_provider');
+  _channel.setMockMethodCallHandler((c) async => '/tmp');
+
+  const channel = MethodChannel('xyz.luan/audioplayers');
+  channel.setMockMethodCallHandler((MethodCall call) async => 1);
+
+  group('AudioPool', () {
+    test('creates instance', () async {
+      final pool =
+          await AudioPool.create(soundPath: 'empty.mp3', maxPlayers: 3);
+      final stop = await pool.start();
+
+      expect((pool.source as AssetSource).path, 'empty.mp3');
+      expect(pool.audioCache.loadedFiles.keys.first, 'empty.mp3');
+      stop();
+      expect((pool.source as AssetSource).path, 'empty.mp3');
+    });
+
+    test('multiple players running', () async {
+      final pool =
+          await AudioPool.create(soundPath: 'empty.mp3', maxPlayers: 3);
+      final stop1 = await pool.start();
+      final stop2 = await pool.start();
+      final stop3 = await pool.start();
+
+      expect((pool.source as AssetSource).path, 'empty.mp3');
+      expect(pool.audioCache.loadedFiles.keys.first, 'empty.mp3');
+      expect(pool.availablePlayers.isEmpty, isTrue);
+      expect(pool.currentPlayers.length, 3);
+
+      await stop1();
+      await stop2();
+      await stop3();
+      expect(pool.availablePlayers.length, 3);
+      expect(pool.currentPlayers.isEmpty, isTrue);
+    });
+
+    test('keeps the minPlayers/maxPlayers contract', () async {
+      final pool =
+          await AudioPool.create(soundPath: 'empty.mp3', maxPlayers: 3);
+      final stopFunctions =
+          await Future.wait(List.generate(5, (_) => pool.start()));
+
+      expect(pool.availablePlayers.isEmpty, isTrue);
+      expect(pool.currentPlayers.length, 5);
+
+      await stopFunctions[0]();
+      await stopFunctions[1]();
+
+      expect(pool.availablePlayers.length, 2);
+      expect(pool.currentPlayers.length, 3);
+
+      await stopFunctions[2]();
+      await stopFunctions[3]();
+      await stopFunctions[4]();
+
+      expect(pool.availablePlayers.length, 3);
+      expect(pool.currentPlayers.isEmpty, isTrue);
+    });
+  });
+}
