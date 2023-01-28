e75ed13d0ce386c750064a59114a564b59db5ce7
Lukas Klingsbo <me@lukas.fyi>
2023-01-28 02:33:13 +0100

    No idea what I'm doing

--- packages/audioplayers/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/audioplayers/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import audioplayers_darwin
 import path_provider_foundation
-import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
