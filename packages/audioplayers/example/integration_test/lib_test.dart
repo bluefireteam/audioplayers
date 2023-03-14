ae274e2996f3c80e7b8067589db2615301dd1fc6
Gustl22 <git@reb0.org>
2023-03-14 15:15:55 +0100

    fix tests

--- packages/audioplayers/example/integration_test/lib_test.dart
+++ packages/audioplayers/example/integration_test/lib_test.dart
@@ -8,7 +8,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'mock_html.dart' if (dart.library.html) 'dart:html';
+import 'mock_html.dart' if (dart.library.html) 'dart:html' show DomException;
 import 'platform_features.dart';
 import 'source_test_data.dart';
 import 'test_utils.dart';
