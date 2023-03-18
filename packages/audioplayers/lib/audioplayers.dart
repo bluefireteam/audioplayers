9f2a2311b1416b2b7a20dbb9adcbf2fc1c41db73
Gustl22 <git@reb0.org>
2023-03-18 14:59:43 +0100

    feat: move audio_context_config to audioplayers

--- packages/audioplayers/lib/audioplayers.dart
+++ packages/audioplayers/lib/audioplayers.dart
@@ -1,4 +1,4 @@
-export 'package:audioplayers_platform_interface/src/api/audio_context_config.dart';
+export 'package:audioplayers_platform_interface/src/api/audio_context.dart';
 export 'package:audioplayers_platform_interface/src/api/global_event.dart';
 export 'package:audioplayers_platform_interface/src/api/player_event.dart';
 export 'package:audioplayers_platform_interface/src/api/player_mode.dart';
@@ -6,6 +6,7 @@ export 'package:audioplayers_platform_interface/src/api/player_state.dart';
 export 'package:audioplayers_platform_interface/src/api/release_mode.dart';
 
 export 'src/audio_cache.dart';
+export 'src/audio_context_config.dart';
 export 'src/audio_pool.dart';
 export 'src/audioplayer.dart';
 export 'src/global_audioplayer.dart';
