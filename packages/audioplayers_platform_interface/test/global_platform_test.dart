ee7fcfea85c836e98b7a54c89dbc6a5858168e79
Gustl22 <git@reb0.org>
2023-04-05 11:03:53 +0200

    test: fix await closing event controller (#1458)
    # Description
    
    Sometimes the tests fail, because the events are emitted after the event
    tests are finished. This awaits the controller to prevent such behavior.

--- packages/audioplayers_platform_interface/test/global_platform_test.dart
+++ packages/audioplayers_platform_interface/test/global_platform_test.dart
@@ -126,7 +126,8 @@ void main() {
         );
       }
 
-      eventController.close();
+      // Await closing controller to avoid handling events after test finishes.
+      await eventController.close();
     });
   });
 }
