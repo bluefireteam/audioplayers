2a0b74d1a41779bffd3a4e04e11845e2ad271985
Gustl22 <git@reb0.org>
2023-05-03 16:51:51 +0200

    Revert "test: debug for linux"
    This reverts commit 9cda452efbc56e5ca66b0c9db07a057e9fd3a535.

--- packages/audioplayers/example/integration_test/lib_test.dart
+++ packages/audioplayers/example/integration_test/lib_test.dart
@@ -70,18 +70,15 @@ void main() {
       (WidgetTester tester) async {
         final players =
             List.generate(audioTestDataList.length, (_) => AudioPlayer());
-        print("Dispose Debug A");
 
         // Start all players simultaneously
         final iterator = List<int>.generate(audioTestDataList.length, (i) => i);
         await Future.wait<void>(
           iterator.map((i) => players[i].play(audioTestDataList[i].source)),
         );
-        print("Dispose Debug B");
         await tester.pumpAndSettle();
         // Sources take some time to get initialized
         await tester.pump(const Duration(seconds: 8));
-        print("Dispose Debug C");
         for (var i = 0; i < audioTestDataList.length; i++) {
           final td = audioTestDataList[i];
           if (td.isLiveStream || td.duration > const Duration(seconds: 10)) {
@@ -92,9 +89,7 @@ void main() {
           }
           await players[i].stop();
         }
-        print("Dispose Debug D");
         await Future.wait(players.map((p) => p.dispose()));
-        print("Dispose Debug E");
       },
       // FIXME: Causes media error on Android (see #1333, #1353)
       // Unexpected platform error: MediaPlayer error with
@@ -104,10 +99,8 @@ void main() {
 
     testWidgets('play multiple sources consecutively',
         (WidgetTester tester) async {
-          print("Dispose Debug F");
       final player = AudioPlayer();
 
-          print("Dispose Debug G");
       for (var i = 0; i < audioTestDataList.length; i++) {
         final td = audioTestDataList[i];
         await player.play(td.source);
@@ -120,10 +113,8 @@ void main() {
           printWithTimeOnFailure('Test position: $td');
           expect(position, greaterThan(Duration.zero));
         }
-        print("Dispose Debug H");
         await player.stop();
       }
-          print("Dispose Debug I");
       await player.dispose();
     });
   });
