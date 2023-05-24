02a4b4b5d71df6fd1ee4c94fa8a0f489a79f8831
Gustl22 <git@reb0.org>
2023-05-24 12:38:50 +0200

    Revert "isWindows"
    This reverts commit 7c41da927924bb76c870d528f4088bbbe932874f.

--- packages/audioplayers/example/integration_test/lib_test.dart
+++ packages/audioplayers/example/integration_test/lib_test.dart
@@ -20,7 +20,6 @@ void main() {
 
   final isAndroid = !kIsWeb && Platform.isAndroid;
   final isLinux = !kIsWeb && Platform.isLinux;
-  final isWindows = !kIsWeb && Platform.isWindows;
 
   final wavUrl1TestData = LibSourceTestData(
     source: UrlSource(wavUrl1),
