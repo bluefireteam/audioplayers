73466b76ebe60ba7a6990d6a78c42e61e6e99d19
Gustl22 <git@reb0.org>
2023-08-02 10:51:52 +0200

    move to platform tests

--- packages/audioplayers/example/integration_test/app/app_source_test_data.dart
+++ packages/audioplayers/example/integration_test/app/app_source_test_data.dart
@@ -23,6 +23,8 @@ class AppSourceTestData extends SourceTestData {
 
 final _features = PlatformFeatures.instance();
 
+// All sources are tested again in lib or platform tests,
+// therefore comment most of them to save testing time
 final audioTestDataList = [
   if (_features.hasUrlSource)
     AppSourceTestData(
@@ -34,11 +36,11 @@ final audioTestDataList = [
       sourceKey: 'url-remote-wav-2',
       duration: const Duration(seconds: 1, milliseconds: 068),
     ),*/
-  if (_features.hasUrlSource)
+  /*if (_features.hasUrlSource)
     AppSourceTestData(
       sourceKey: 'url-remote-mp3-1',
       duration: const Duration(minutes: 3, seconds: 30, milliseconds: 77),
-    ),
+    ),*/
   /*if (_features.hasUrlSource)
     AppSourceTestData(
       sourceKey: 'url-remote-mp3-2',
@@ -56,22 +58,22 @@ final audioTestDataList = [
       duration: Duration.zero,
       isLiveStream: true,
     ),*/
-  if (_features.hasAssetSource)
+  /*if (_features.hasAssetSource)
     AppSourceTestData(
       sourceKey: 'asset-wav',
       duration: const Duration(seconds: 1, milliseconds: 068),
-    ),
+    ),*/
   /*if (_features.hasAssetSource)
     AppSourceTestData(
       sourceKey: 'asset-mp3',
       duration: const Duration(minutes: 1, seconds: 34, milliseconds: 119),
-    ),
-  if (_features.hasBytesSource)
+    ),*/
+  /*if (_features.hasBytesSource)
     AppSourceTestData(
       sourceKey: 'bytes-local',
       duration: const Duration(seconds: 1, milliseconds: 068),
-    ),
-  if (_features.hasBytesSource)
+    ),*/
+  /*if (_features.hasBytesSource)
     AppSourceTestData(
       sourceKey: 'bytes-remote',
       duration: const Duration(minutes: 3, seconds: 30, milliseconds: 76),
