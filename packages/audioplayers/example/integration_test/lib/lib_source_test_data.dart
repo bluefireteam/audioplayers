36c3979dc2aba88dc720150151242180d7649bc6
Gustl22 <git@reb0.org>
2023-08-02 09:43:06 +0200

    remove redundant resources

--- packages/audioplayers/example/integration_test/lib/lib_source_test_data.dart
+++ packages/audioplayers/example/integration_test/lib/lib_source_test_data.dart
@@ -1,6 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers_example/tabs/sources.dart';
-import 'package:http/http.dart';
 
 import '../platform_features.dart';
 import '../source_test_data.dart';
@@ -37,49 +36,52 @@ final mp3Url1TestData = LibSourceTestData(
   duration: const Duration(minutes: 3, seconds: 30, milliseconds: 77),
 );
 
-Future<List<LibSourceTestData>> getAudioTestDataList() async => [
-  if (_features.hasUrlSource) wavUrl1TestData,
-  if (_features.hasUrlSource)
-    LibSourceTestData(
-      source: UrlSource(wavUrl2),
-      duration: const Duration(seconds: 1, milliseconds: 068),
-    ),
-  if (_features.hasUrlSource) mp3Url1TestData,
-  if (_features.hasUrlSource)
-    LibSourceTestData(
-      source: UrlSource(mp3Url2),
-      duration: const Duration(minutes: 1, seconds: 34, milliseconds: 119),
-    ),
-  if (_features.hasUrlSource && _features.hasPlaylistSourceType)
-    LibSourceTestData(
-      source: UrlSource(m3u8StreamUrl),
-      duration: Duration.zero,
-      isLiveStream: true,
-    ),
-  if (_features.hasUrlSource)
-    LibSourceTestData(
-      source: UrlSource(mpgaStreamUrl),
-      duration: Duration.zero,
-      isLiveStream: true,
-    ),
-  if (_features.hasAssetSource)
-    LibSourceTestData(
-      source: AssetSource(wavAsset),
-      duration: const Duration(seconds: 1, milliseconds: 068),
-    ),
-  if (_features.hasAssetSource)
-    LibSourceTestData(
-      source: AssetSource(mp3Asset),
-      duration: const Duration(minutes: 1, seconds: 34, milliseconds: 119),
-    ),
-  if (_features.hasBytesSource)
-    LibSourceTestData(
-      source: BytesSource(await AudioCache.instance.loadAsBytes(wavAsset)),
-      duration: const Duration(seconds: 1, milliseconds: 068),
-    ),
-  if (_features.hasBytesSource)
-    LibSourceTestData(
-      source: BytesSource(await readBytes(Uri.parse(mp3Url1))),
-      duration: const Duration(minutes: 3, seconds: 30, milliseconds: 76),
-    ),
-];
+// Some sources are commented which are considered redundant
+Future<List<LibSourceTestData>> getAudioTestDataList() async {
+  return [
+    if (_features.hasUrlSource) wavUrl1TestData,
+    /*if (_features.hasUrlSource)
+      LibSourceTestData(
+        source: UrlSource(wavUrl2),
+        duration: const Duration(seconds: 1, milliseconds: 068),
+      ),*/
+    if (_features.hasUrlSource) mp3Url1TestData,
+    /*if (_features.hasUrlSource)
+      LibSourceTestData(
+        source: UrlSource(mp3Url2),
+        duration: const Duration(minutes: 1, seconds: 34, milliseconds: 119),
+      ),*/
+    if (_features.hasUrlSource && _features.hasPlaylistSourceType)
+      LibSourceTestData(
+        source: UrlSource(m3u8StreamUrl),
+        duration: Duration.zero,
+        isLiveStream: true,
+      ),
+    if (_features.hasUrlSource)
+      LibSourceTestData(
+        source: UrlSource(mpgaStreamUrl),
+        duration: Duration.zero,
+        isLiveStream: true,
+      ),
+    if (_features.hasAssetSource)
+      LibSourceTestData(
+        source: AssetSource(wavAsset),
+        duration: const Duration(seconds: 1, milliseconds: 068),
+      ),
+    /*if (_features.hasAssetSource)
+      LibSourceTestData(
+        source: AssetSource(mp3Asset),
+        duration: const Duration(minutes: 1, seconds: 34, milliseconds: 119),
+      ),*/
+    if (_features.hasBytesSource)
+      LibSourceTestData(
+        source: BytesSource(await AudioCache.instance.loadAsBytes(wavAsset)),
+        duration: const Duration(seconds: 1, milliseconds: 068),
+      ),
+    /*if (_features.hasBytesSource)
+      LibSourceTestData(
+        source: BytesSource(await readBytes(Uri.parse(mp3Url1))),
+        duration: const Duration(minutes: 3, seconds: 30, milliseconds: 76),
+      ),*/
+  ];
+}
