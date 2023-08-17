1f8d23a1911e586cd7022bc589a0ec73bedab7fb
Gustl22 <git@reb0.org>
2023-08-17 17:05:08 +0200

    feat: create, dispose & reuse event stream (#1609)
    # Description
    
    Currently if an event stream for a `playerId` is requested, a new stream
    was created, conflicting with the existing one. Now the
    `createEventStream`, `disposeEventStream` and `getEventStream` methods
    allow more control of handling the stream and also reusing the existing
    stream on `getEventStream`.

--- packages/audioplayers_platform_interface/test/util.dart
+++ packages/audioplayers_platform_interface/test/util.dart
@@ -7,6 +7,17 @@ extension MethodCallParser on MethodCall {
   Map<Object?, Object?> get args => arguments as Map<Object?, Object?>;
 }
 
+void createNativeMethodHandler({
+  required String channel,
+  Future<Object?>? Function(MethodCall message)? handler,
+}) {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    MethodChannel(channel),
+    handler,
+  );
+}
+
 // See: https://github.com/flutter/packages/blob/12609a2abbb0a30b9d32af7b73599bfc834e609e/packages/video_player/video_player_android/test/android_video_player_test.dart#L270
 void createNativeEventStream({
   required String channel,
