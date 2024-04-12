39a01227969e2510279f500c343c02fc3f89c337
Gustl22 <git@reb0.org>
2024-04-12 16:05:57 +0200

    ReleaseMode.release (Linux)

--- packages/audioplayers_linux/linux/audio_player.cc
+++ packages/audioplayers_linux/linux/audio_player.cc
@@ -249,10 +249,12 @@ void AudioPlayer::OnPlaybackEnded() {
     fl_value_set_string(map, "value", fl_value_new_bool(true));
     fl_event_channel_send(this->_eventChannel, map, nullptr, nullptr);
   }
-  if (GetLooping()) {
-    Play();
+  if (GetReleaseMode() == ReleaseMode::loop) {
+      Play();
+  } else if (GetReleaseMode() == ReleaseMode::release) {
+      ReleaseMediaSource();
   } else {
-    Stop();
+      Stop();
   }
 }
 
@@ -280,12 +282,12 @@ void AudioPlayer::SetBalance(float balance) {
   g_object_set(G_OBJECT(panorama), "panorama", balance, NULL);
 }
 
-void AudioPlayer::SetLooping(bool isLooping) {
-  _isLooping = isLooping;
+void AudioPlayer::SetReleaseMode(ReleaseMode releaseMode) {
+  _releaseMode = releaseMode;
 }
 
-bool AudioPlayer::GetLooping() {
-  return _isLooping;
+ReleaseMode AudioPlayer::GetReleaseMode() {
+  return _releaseMode;
 }
 
 void AudioPlayer::SetVolume(double volume) {
