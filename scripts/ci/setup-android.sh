e679085314466e2ed3e37074f1c80af29585c63f
Gustl22 <git@reb0.org>
2025-03-02 14:49:23 +0100

    sleep

--- scripts/ci/setup-android.sh
+++ scripts/ci/setup-android.sh
@@ -36,6 +36,6 @@ echo "no" | "$ANDROID_CMDLINE_TOOLS"/avdmanager create avd --force --name emu --
 # Start Emulator
 echo "Starting emulator"
 nohup "$ANDROID_HOME"/emulator/emulator -avd emu -no-audio -no-snapshot -no-window &
-"$ANDROID_HOME"/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+"$ANDROID_HOME"/platform-tools/adb wait-for-device shell "while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82"
 "$ANDROID_HOME"/platform-tools/adb devices
 echo "Emulator started"
