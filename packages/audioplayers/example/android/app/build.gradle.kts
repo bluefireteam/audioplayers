0cccf1489a3540167795bc92e0aea7fdad1522b9
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2025-08-26 16:49:43 +0200

    Set NDK version

--- packages/audioplayers/example/android/app/build.gradle.kts
+++ packages/audioplayers/example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.audioplayers_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
