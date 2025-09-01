0fb1f29da03a03cebd4111257ac3f8f973812e56
Gustl22 <git@reb0.org>
2025-09-01 15:33:47 +0200

    make example runnable

--- packages/audioplayers/example/android/app/build.gradle.kts
+++ packages/audioplayers/example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.audioplayers_example"
+    namespace = "xyz.luan.audioplayers.example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.audioplayers_example"
+        applicationId = "xyz.luan.audioplayers.example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
