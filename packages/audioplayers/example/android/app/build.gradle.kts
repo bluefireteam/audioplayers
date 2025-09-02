fd6d67366936a7e348ca912478f1e28c9feaf92b
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2025-09-02 08:49:42 +0200

    chore: Upgrade android example versions (#1944)
    # Description
    
    This upgrades the gradle versions and some related versions for the
    example, and moves to kotlin gradle files.
    
    ## Checklist
    
    <!-- Before you create this PR confirm that it meets all requirements
    listed below by checking the
    relevant checkboxes (`[x]`). This will ensure a smooth and quick review
    process. -->
    
    - [x] The title of my PR starts with a [Conventional Commit] prefix
    (`fix:`, `feat:`, `refactor:`,
          `docs:`, `chore:`, `test:`, `ci:` etc).
    - [x] I have read the [Contributor Guide] and followed the process
    outlined for submitting PRs.
    - [x] I have updated/added tests for ALL new/updated/fixed
    functionality.
    - [x] I have updated/added relevant documentation and added dartdoc
    comments with `///`, where necessary.
    - [x] I have updated/added relevant examples in [example].
    
    ## Breaking Change
    
    <!-- Does your PR require audioplayers users to manually update their
    apps to accommodate your change?
    
    If the PR is a breaking change this should be indicated with suffix "!" 
    (for example, `feat!:`, `fix!:`). See [Conventional Commit] for details.
    -->
    
    - [ ] Yes, this is a breaking change.
    - [x] No, this is *not* a breaking change.
    
    <!-- If the PR is breaking, uncomment the following section and add
    instructions for how to migrate from
    the currently released version to the new proposed way. -->
    
    <!--
    ### Migration instructions
    
    Before:
    ```
    ```
    
    After:
    ```
    ```
    -->
    
    ## Related Issues
    
    <!-- Provide a list of issues related to this PR from the [issue
    database].
    Indicate which of these issues are resolved or fixed by this PR, i.e.
    Fixes #xxx !-->
    
    <!-- Links -->
    [issue database]: https://github.com/bluefireteam/audioplayers/issues
    [Contributor Guide]:
    https://github.com/bluefireteam/audioplayers/blob/main/contributing.md#feature-requests--prs
    [Conventional Commit]: https://conventionalcommits.org
    [example]:
    https://github.com/bluefireteam/audioplayers/tree/main/packages/audioplayers/example
    
    ---------
    
    Co-authored-by: Gustl22 <git@reb0.org>

---
+++ packages/audioplayers/example/android/app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "xyz.luan.audioplayers.example"
+    // [Audioplayers] We try to use the values specified by Flutter, so compatible versions are used out of the box.
+    compileSdk = flutter.compileSdkVersion
+    // The prompt to upgrade the NDK version usually is just a warning and can be ignored.
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "xyz.luan.audioplayers.example"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
