9a11a7d3bec6eba3f4572c802f866ec31cad1a25
Rexios <rexios@rexios.dev>
2026-04-15 15:33:13 -0400

    Update comment

--- packages/audioplayers/lib/src/audio_cache.dart
+++ packages/audioplayers/lib/src/audio_cache.dart
@@ -128,7 +128,7 @@ class AudioCache {
   Future<Uri> load(String fileName) async {
     var needsFetch = !loadedFiles.containsKey(fileName);
 
-    // On Android, verify that the cached file still exists. It can be removed
+    // Verify that the cached file still exists. It can be removed
     // by the system when the storage is almost full
     // see https://developer.android.com/training/data-storage/app-specific#internal-remove-cache
     if (!needsFetch && !await fileSystem.file(loadedFiles[fileName]).exists()) {
